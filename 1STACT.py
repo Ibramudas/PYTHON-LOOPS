5e3a5f3c360ba5563c0cb08633f534b6b80ef29c
Ibramudas <ibramudas2@gmail.com>
2025-01-02 19:06:07 +0100

    python 1

---
+++ 1STACT.py
@@ -0,0 +1,6 @@
+var = 22
+print("Tables of 22")
+for i in range (1,11):
+    mul = var*i
+    print(f"22*{i}={mul}")
+
---
+++ 3rd act.py
@@ -0,0 +1,8 @@
+num = 4
+sum = 0
+while(num<=10):
+  sum += num
+  num = num + 1
+  
+
+print("The total sum of the natural numbers is", sum)
